0e9576a11f8554eab698abfbe6bc06fef293eb20
Ryuuabcderf <nguyenduchanh99tnvn@gmail.com>
2020-04-15 16:17:14 +0700

    fix jwt

--- prosocial/views.py
+++ prosocial/views.py
@@ -62,3 +62,20 @@ class TickViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Tick.objects.all()
     serializer_class = TickSerializer
+
+# custom TokenObtain view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        # print(user)
+        token = super().get_token(user)
+        # print(type(token))
+        token['id'] = user.id
+        # print(token)
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
