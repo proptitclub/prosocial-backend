71103d5bddd63bb8567c31b96a844b852a55a68d
Ryuuabcderf <nguyenduchanh99tnvn@gmail.com>
2020-09-21 21:14:42 +0700

    update

--- prosocial/curl.py
+++ prosocial/curl.py
@@ -1,4 +1,3 @@
-curl -X POST -H "Content-Type: application/json" -d '{"callbot_id":"15","callcenter_phone":"0366568956","customer_phone":"0387697588","customer_area":"CENTRAL","input_slots":{"name":"NGUYEN VAN HAI","pronoun":"anh","date": "20/07/2020","location": "Vpbank Láng Hạ","hotline": "1900545415","location_detail": "Số 25 đường Phạm Văn Đồng, thành phố Hà Nội"
-}}' http://127.0.0.1:5080/api/call/create
+curl -X POST -H "Content-Type: application/json" -d '{"callbot_id":"25","callcenter_phone":"0366568956","customer_phone":"0387697588","customer_area":"CENTRAL","input_slots":{"date": "20/07/2020"}}' http://127.0.0.1:5080/api/call/create
 
-curl -X POST -H "Content-Type: application/json" -d '{"conversation_id":"20200909105923-7d846cd6-2d2b-4ce5-b04c-4d712db28933"}' http://127.0.0.1:5080/api/call/callout_safe
+curl -X POST -H "Content-Type: application/json" -d '{"conversation_id":"20200921105436-1c333ce5-8d0e-48e7-a620-90c95960a3d1"}' http://127.0.0.1:5080/api/call/callout_safe
--- prosocial/views.py
+++ prosocial/views.py
@@ -173,14 +173,14 @@ class PostViewSet(viewsets.ModelViewSet):
         # new_notification.save()
         # user_list = new_post.assigned_group.members
 
-        # polls = request.data.get('polls')
-        # # print(polls)
-        # dict_poll_data = json.loads(polls)
-        # for poll_data in dict_poll_data:
-        #     print(content)
-        #     content = poll_data
-        #     new_poll = Poll(assigned_post=new_post, question=content)
-        #     new_poll.save()
+        polls = request.data.get('polls')
+        # print(polls)
+        dict_poll_data = json.loads(polls)
+        for poll_data in dict_poll_data:
+            print(content)
+            content = poll_data
+            new_poll = Poll(assigned_post=new_post, question=content)
+            new_poll.save()
         
         # relation_device_id_list = []
         # for user in user_list.all():
