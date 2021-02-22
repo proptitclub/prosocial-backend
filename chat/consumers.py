e5ce41694734b3eafa0880befb914111ddd707be
Ryuuabcderf <nguyenduchanh99tnvn@gmail.com>
2021-02-22 15:23:57 +0700

    delete relation channels

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,5 @@
 # chat/consumers.py
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
 from .models import *
 from prosocial.models import *
 import socketio
